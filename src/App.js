10ab8266d49de47401bb2ab01a797e4ab35a7dd2
Hamid Samimi <samimi.electronics@gmail.com>
2020-09-14 13:03:22 +0430

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
+import TodoList from "./components/TodoList";
+import * as shortid from 'shortid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    /* Create local unique ID */
+    const id = shortid
+
+    /* Todos State */
+    const [todos, setTodos] = useState([])
+
+    /* TODO list input reference */
+    const todoInputRef = useRef()
+
+    /* Load stored todos to setTodos in the application */
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+        if (storedTodos) setTodos(storedTodos)
+    }, [])
+
+    /* Store current page todos to local storage */
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos])
+
+    /* change todo state */
+    function toggleTodo(id) {
+        /* In react, never modify the original state.
+        * Always make a copy */
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id)
+        todo.complete = !todo.complete
+        setTodos(newTodos)
+    }
+
+    /* Handler for add todo button */
+    function handleOnTodo(e) {
+        const name = todoInputRef.current.value
+        if (name === '') return
+        setTodos(prevTodos => {
+            return [...prevTodos, {id: id.generate(), name, completed: false}]
+        })
+        todoInputRef.current.value = null
+    }
+
+    function handleOnClearComplete(e) {
+        const newTodos = todos.filter(todos => !todos.complete)
+        setTodos(newTodos)
+    }
+
+    /* Return application */
+    return (
+      <>
+          <TodoList todos = { todos } toggleTodo={toggleTodo}/>
+          <input ref={ todoInputRef } type="text" />
+          <button onClick={handleOnTodo}>Add To-Do</button>
+          <button onClick={handleOnClearComplete}>Clear completed</button><br/>
+          <span>{todos.filter(todo => !todo.complete).length} left to-do</span>
+      </>
+    );
 }
 
 export default App;
